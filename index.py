aae26a9aa821464d288c2136e87852c18ba8f4b0
Bruno Felipe <brunomafraf@gmail.com>
2026-02-04 10:52:53 -0300

    Adicionado, categorias, graficos, e mais...

--- index.py
+++ index.py
@@ -7,9 +7,9 @@ import os
 def criar_carregar():
     planilha = "Minhas_Contas.xlsx" 
     if os.path.exists(planilha):
-        return pd.read_excel (planilha, usecols=["Descricao","Categoria", "Valor", "Vencimento", "Status"])
+        return pd.read_excel (planilha, usecols=["Tipo","Descricao","Categoria", "Valor", "Vencimento", "Status"])
     else:
-        return pd.DataFrame(columns=["Descricao","Categoria", "Valor", "Vencimento", "Status"])
+        return pd.DataFrame(columns=["Tipo","Descricao","Categoria", "Valor", "Vencimento", "Status"])
     
 
 def tratar_data():
@@ -29,30 +29,85 @@ def tratar_data():
 
 def adicionar_gasto():
 
+    categorias={"Saidas":['Transporte','Lazer','Investimento','Fixo','NaN'],"Entradas":['Pagamentos','Presentes','Rendimentos']}
 
     df= criar_carregar()
+
+    tipo=int(input("Qual o tipo do valor?\n"
+                   "[1] Entrada\n"
+                   "[2] Saida\n"
+                   "Escolha: "))
+    
     desc = input("Descrição (ex: Gasolina): ").capitalize()
-    categ= input("Categorias:\n" \
-    "1 - Transporte\n"
-    "2 - Lazer\n"
-    "3 - Investimento\n"
-    "4 - Fixo\n"
-    "Escolha: ")
-    valor = float(input("Valor (ex: 150.00): ")) 
+
+    if tipo == 1:
+        tipo='Entrada'
+        categ_salva=categorias['Entradas']
+        for n, c in enumerate(categorias["Entradas"]):
+            print(f'{n+1} - {c}')
+
+    elif tipo == 2:
+        tipo='Saida'
+        categ_salva=categorias['Saidas']
+        for n, c in enumerate(categorias["Saidas"]):
+            print(f'{n+1} - {c}')
+
+    while True:
+        try:
+            categ= int(input("Escolha a categoria: "))
+            if len(categ_salva)< categ or categ <=0:
+                print(f"Erro: Escolha um número entre 1 e {len(categ_salva)}")
+                continue 
+            break
+        except:
+            print("Opção Inválida! Somente números de [1 á 5].")
+
+    categ_salva=categ_salva[categ-1]
+
+    while True:
+        try:
+            valor = float(input("Valor (ex: 150.00): ")) 
+            break
+        except:
+            print("Opção Inválida! Somente números.")
+
     data=tratar_data()
+
     status = input("Status (Pago/Pendente): ").lower().capitalize()
+            
 
     nova_linha={
+        'Tipo':tipo,
         'Descricao':desc,
-        'Categoria':categ,
+        'Categoria':categ_salva,
         'Valor':valor,
         'Vencimento':data,
         'Status':status
     }
 
     df = df._append(nova_linha, ignore_index=True)
-    df['Vencimento']= pd.to_datetime(df['Vencimento'])
+    df.to_excel("Minhas_Contas.xlsx", index=False, engine='openpyxl')
+    mostrar_grafico=(input("Você quer ver um gráfico sobre as contas?\n" \
+    "[1] - Gráfico de gastos por setor\n"
+    '[2] - Gráfico de Gastos Gerais\n' \
+    '[3] - Balanço Mensal (lucro ou Prejuizo)\n'
+    'Escolha [Deixe em branco para não ver]: '))
+    if mostrar_grafico != '':
+        mostrar_grafico= int(mostrar_grafico)
+        if mostrar_grafico == 1:
+            grafico_gastos_setor(df)
+        elif mostrar_grafico ==2:
+            grafico_gastos_gerais(df)
+        elif mostrar_grafico==3:
+            grafico_lucro(df)
+    else:
+        print("Opção inválida! Prosseguindo...")
+        
+    formatar()
 
+def grafico_gastos_gerais(df):
+    
+    df['Vencimento']= pd.to_datetime(df['Vencimento'])
     gastos_pagos=df[df['Status']=='Pago']['Valor'].sum()
     gastos_pendentes=df[df["Status"]=='Pendente']['Valor'].sum()
     categories=['Pago','Pendente']
@@ -61,9 +116,37 @@ def adicionar_gasto():
     plt.title("Status das Contas - Janeiro 2026")
     plt.ylabel('Valor (R$)')
     plt.show()
-    planilha = "Minhas_Contas.xlsx" 
-    df.to_excel(planilha, index=False, engine='openpyxl')
-    formatar()
+
+def grafico_gastos_setor(df):
+    if df.empty:
+        return
+    soma_por_categoria= df.groupby('Categoria')["Valor"].sum()
+    plt.figure()
+    soma_por_categoria.plot(kind="pie", autopct="%1.1f%%")
+    plt.title("Gastos Por Setor")
+    plt.ylabel('')
+    plt.show()
+
+def grafico_lucro(df):
+    if df.empty:
+        return
+    soma_entrada=df[df['Tipo'] == 'Entrada']['Valor'].sum()
+    soma_saida=df[df['Tipo'] == 'Saida']['Valor'].sum()
+    saldo=soma_entrada-soma_saida
+    nome=['Entradas','Saídas']
+    valores=[soma_entrada,soma_saida]
+    plt.figure(figsize=(6,5))
+    plt.bar(nome,valores,color=["green", 'red'])
+    if saldo >= 0:
+        cor_saldo = 'green'
+        texto_saldo = f"LUCRO: R$ {saldo:.2f}"
+    else:
+        cor_saldo = 'red'
+        texto_saldo = f"PREJUÍZO: R$ {saldo:.2f}"
+    plt.title(f"Balanço do Mês\n{texto_saldo}",color=cor_saldo,fontweight='bold')
+
+    plt.ylabel('Valor (R$)')
+    plt.show()
 
 def formatar():
     planilha = "Minhas_Contas.xlsx" 
